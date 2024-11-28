e417118d726c80bd83fe81d04068a06dd9669f14
Tatiana Konischeva <moyzaldi@gmail.com>
2024-11-28 12:39:34 +0300

    Заданы размеры окна браузера

--- demoqa-tests/conftest.py
+++ demoqa-tests/conftest.py
@@ -8,6 +8,8 @@ def browser_settings():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
     browser.config.base_url = 'https://demoqa.com'
 
     yield
